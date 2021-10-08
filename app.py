4fdc0548e68f7068569471b0bddf180e53da46cb
lazarod07 <cristianlazarod@gmail.com>
2021-10-07 22:17:00 -0500

    Integracion index and login

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+@app.route('/index')
+@app.route('/inicio')
+def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
