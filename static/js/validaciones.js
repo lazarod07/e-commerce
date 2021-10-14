08a40f589ca853a09b1fd85f1f2e21b0a0d3ba57
Marcela Gonzalez D <marcelagonzalez@uninorte.edu.co>
2021-10-14 10:24:56 -0500

    Adicion validaciones login y registro

--- static/js/validaciones.js
+++ static/js/validaciones.js
@@ -10,6 +10,57 @@ function mostrarPassword(){
         obj.type = "password";
         ojito.classList.toggle("far fa-eye");
         
+    }    
+}
+
+function validar_form_login(){
+     
+    correoRegistro=document.getElementById("correo").value;
+    passwordRegistro=document.getElementById("password").value;
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+    if ( correoRegistro == "" ){        
+        alert(" El campo del correo electrónico no debe estar vacío.");
+        //return false;
+    }else if ( expReg.test(correoRegistro) == false ){
+        alert("No es un correo elctrónico válido.");    
+        //return false;
+    }else if ( passwordRegistro == "" ){
+        alert("El campo Contraseña no debe estar vacío.");
+        //return false;
+    }else if ( passwordRegistro.length < 8 ){
+        alert("El campo Contraseña debe tener mínimo 8 caracteres");
+        //return false;
     }
+
+}
+
+function validar_form_registro(){
     
+    nombresRegistro=document.getElementById("nombres").value;
+    apellidosRegistro=document.getElementById("apellidos").value;
+    correoRegistro=document.getElementById("correo").value;
+    passwordRegistro=document.getElementById("password").value;
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    
+    if ( nombresRegistro == "" ){
+        alert("El campo Nombres no debe estar vacío.");
+        return false;
+    }else if ( apellidosRegistro == "" ){
+        alert("El campo Apellidos no debe estar vacío.");
+        return false;
+    }else if ( correoRegistro == "" ){        
+        alert(" El campo del correo electrónico no debe estar vacío.");
+        return false;
+    }else if ( expReg.test(correoRegistro) == false ){
+        alert("No es un correo elctrónico válido.");    
+        return false;
+    }else if ( passwordRegistro == "" ){
+        alert("El campo Contraseña no debe estar vacío.");
+        return false;
+    }else if ( passwordRegistro.length < 8 ){
+        alert("El campo Contraseña debe tener mínimo 8 caracteres");
+        return false;
+    }
+
 }
